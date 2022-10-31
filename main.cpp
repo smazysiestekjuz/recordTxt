ea0e35b0bb964eaedc1bdbdfe66d5e4a83e8379d
Mateusz Pniewski <smazysiestekjuz@gmail.com>
2022-10-31 07:02:56 +0100

    Add library to the program and creat variables

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+string imie, nazwisko;
+int nr_tel;
+
+int main()
+{
+    cout << "Podaj imie: ";         cin>>imie;
+    cout << "Podaj nazwisko: ";     cin>>nazwisko;
+    cout << "Podaj nr telefonu: ";  cin>>nr_tel;
+
+    return 0;
+}
